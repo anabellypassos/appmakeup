b9fc66b75088634ea7db0555ec52e17d57adbe00
Anabelly <anabellybel123@gmail.com>
2024-07-29 12:44:35 -0300

    tela inicial de carregamento

---
+++ lib/telacarregamento/telacareggamneto_principal.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class TelacareggamentoPrincipal extends StatelessWidget {
+  const TelacareggamentoPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/image/1103tr1301_5c.jpeg',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Glam Guru',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 32,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Colors.pink.withOpacity(0.5),
+                        offset: const Offset(2.0, 2.0),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30), // Espaço entre o texto e o botão
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color.fromARGB(255, 236, 234, 235).withOpacity(0.5),
+                        blurRadius: 10.0,
+                        offset: const Offset(2.0, 2.0),
+                      ),
+                    ],
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Adicione a funcionalidade do botão aqui
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 238, 126, 164),
+                    ),
+                    child: const Text(
+                      'Começar',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
